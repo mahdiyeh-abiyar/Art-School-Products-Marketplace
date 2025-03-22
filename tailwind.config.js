a2137ba9a9c90e092c61748e0b008ffd7db52d51
mahdiyeh-abiyar <mahdiehabyari@gmail.com>
2025-03-23 02:41:12 +0330

    chore: initialize project structure and install Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#FE4F2D",
+        "dark-teal": "#015551",
+        teal: "#57B4BA",
+        cream: "#FDFBEE",
+        "gray-blue": "#9AA6B2",
+        "dark-gray": "#45474B",
+        "light-gray": "#F5F7F8",
+        "bright-orange": "#FF3B15",
+      },
+    },
+  },
+  plugins: [],
+}
