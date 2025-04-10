649e1f5f5630e64b4db6f29b715e69c5464dc4df
Nazanin <nazaninbakhshi386@gmail.com>
2025-04-10 22:04:19 +0330

    chore(backend): organize file structure and add static folder

--- Art_School_Products_Marketplace/Art_School_Products_Marketplace/settings.py
+++ Art_School_Products_Marketplace/Art_School_Products_Marketplace/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Art_School_Products_Marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# Media Files Config
+MEDIA_ULR = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
