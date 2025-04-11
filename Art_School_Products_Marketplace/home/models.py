be8baf9401ca414e5a973084323a02e5f9cb636f
SmilingParnia <katsukidekubakugo@gmail.com>
2025-04-11 21:31:58 +0330

    create a table in Home db
    just create without any data

--- Art_School_Products_Marketplace/home/models.py
+++ Art_School_Products_Marketplace/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Home(models.Model):
+    Title = models.CharField(max_length = 200)
+    Text = models.TextField(max_length = 10000)
+    # images = models.ImageField(upload_to = ".")
