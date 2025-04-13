ae23d3e0244aa1b1f41bd42fceb0ee81dbb3defe
Zahra-1386 <zahra86hamidi@gmail.com>
2025-04-13 12:57:49 -0700

    Add login form and script functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+function togglePassword() {
+    const passwordField = document.getElementById("password");
+    const eyeIcon = document.getElementById("togglePassword");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
