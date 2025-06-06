49af0a40d36af04b8529e5f0d193005ef68a3c72
Nazanin <nazaninbakhshi386@gmail.com>
2025-06-06 20:13:00 +0330

    fix: protect dashboard route, update footer links, enhance product list and routing

--- Art_School_Products_Marketplace/dashboard/views.py
+++ Art_School_Products_Marketplace/dashboard/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def dashboard_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login:login_page')
     return render(request, 'dashboard/Dashboard.html')
 
 def user_dashboard(request):
+    if not request.user.is_authenticated:
+        return redirect('login:login_page')
     return render(request, 'dashboard/user_dashboard.html')
 
 def order_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login:login_page')
     return render(request, 'dashboard/order.html')
 
 def Favorites_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login:login_page')
     return render(request, 'dashboard/Favorites.html')
