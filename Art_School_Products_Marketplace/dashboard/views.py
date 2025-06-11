8527bd29dc907dfbd07dd786eb01c9fc10f3378a
Nazanin <nazaninbakhshi386@gmail.com>
2025-06-11 08:52:16 +0330

    feat: add conditional footer rendering with 'hide_footer' context variable

--- Art_School_Products_Marketplace/dashboard/views.py
+++ Art_School_Products_Marketplace/dashboard/views.py
@@ -4,19 +4,31 @@ from django.shortcuts import render, redirect
 def dashboard_page(request):
     if not request.user.is_authenticated:
         return redirect('login:login_page')
-    return render(request, 'dashboard/Dashboard.html')
+    context = {
+        'hide_footer': True
+    }
+    return render(request, 'dashboard/Dashboard.html', context)
 
 def user_dashboard(request):
     if not request.user.is_authenticated:
         return redirect('login:login_page')
-    return render(request, 'dashboard/user_dashboard.html')
+    context = {
+        'hide_footer': True
+    }
+    return render(request, 'dashboard/user_dashboard.html', context)
 
 def order_page(request):
     if not request.user.is_authenticated:
         return redirect('login:login_page')
-    return render(request, 'dashboard/order.html')
+    context = {
+        'hide_footer': True
+    }
+    return render(request, 'dashboard/order.html', context)
 
 def Favorites_page(request):
     if not request.user.is_authenticated:
         return redirect('login:login_page')
-    return render(request, 'dashboard/Favorites.html')
+    context = {
+        'hide_footer': True
+    }
+    return render(request, 'dashboard/Favorites.html', context)
